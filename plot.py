70af924513286c8e0a725c6e77f87d46d2bc3381
Rafael R <rafael.ruggiero@usp.br>
2017-10-21 00:08:09 +0200

    updated version

--- plot.py
+++ plot.py
@@ -3,13 +3,17 @@ import datashader as ds
 import datashader.transfer_functions as tf
 from datashader.colors import Greys9, Hot, colormap_select as cm 
 
-def bg(img): return tf.set_background(img,"black")
+pixels=800 # size of image
+filename='pos.csv' # name of input file
 
-df = pd.read_csv('pos.csv')
+def bg(img):
+  return tf.set_background(img,"black")
+
+df = pd.read_csv(filename)
 
 cvs = ds.Canvas(plot_width=pixels, plot_height=pixels)
 agg = cvs.points(df, 'x_col', 'y_col')
 
-img = bg(tf.interpolate(agg, cmap = cm(Greys9,0.2), how='log')) 
+img = bg(tf.interpolate(agg, cmap = cm(Greys9,0.2), how='log'))
 img2 = img.to_pil()
 img2.save('output.png')
